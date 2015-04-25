3f41c62296871592c4a98bad8d472e9e32e4a17e
Patrik Bóna <patrik.bona@mrhead.sk>
2015-04-25 21:09:48 +0200

    Initial version with directory support

---
+++ splitter.rb
@@ -0,0 +1,61 @@
+#!/usr/bin/env ruby
+
+class DirSplitter
+  attr_reader :directory
+  def initialize(directory)
+    @directory = directory
+  end
+
+  def split_dcim_files
+    Dir["#{directory}/*.dcim"].each do |file|
+      split_file(file)
+    end
+  end
+
+  private
+
+  def split_file(file)
+    print "Processing #{file}... "
+    output_directory = file.gsub(/\.dcim/, '')
+
+    if File.directory?(output_directory)
+      puts "skipped because #{output_directory} already exists."
+      next
+    else
+      Dir.mkdir(output_directory)
+    end
+
+    Splitter.new(file, output_directory).split_to_files
+    puts "done."
+  end
+end
+
+class Splitter
+  SEPARATOR = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00DICM\x02\x00"
+
+  attr_reader :file_in_string
+
+  def initialize(file, output_dir = nil)
+    @file_in_string = File.binread(file)
+    @output_dir = output_dir || File.basename(file)
+  end
+
+  def split_to_files
+    file_parts.each_with_index do |file_part, index|
+      f = File.new("#{output_dir}/file_#{index}", 'w')
+      file_content = SEPARATOR + file_part
+      f.write(file_content)
+      f.close
+    end
+  end
+
+  def file_parts
+    file_in_string.split(SEPARATOR)
+  end
+end
+
+if ARGV.size == 1
+  DirSplitter.new(ARGV[0]).split_dcim_files
+else
+  puts "Usage: ./splitter.rb <directory>"
+end
