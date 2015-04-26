80ba3e0952fb974afd1b63b164b48cd824331a27
Patrik Bóna <patrik.bona@mrhead.sk>
2015-04-26 13:50:08 +0200

    Look for files recursively

--- splitter.rb
+++ splitter.rb
@@ -1,13 +1,17 @@
 #!/usr/bin/env ruby
 
+require 'fileutils'
+
 class DirSplitter
-  attr_reader :directory
-  def initialize(directory)
-    @directory = directory
+  attr_reader :input_directory, :output_directory_base
+
+  def initialize(input_directory, output_directory_base)
+    @input_directory = input_directory
+    @output_directory_base = output_directory_base
   end
 
   def split_dcim_files
-    Dir["#{directory}/*"].each do |file|
+    Dir.glob("#{input_directory}/**/*").each do |file|
       split_file(file) if File.file?(file)
     end
   end
@@ -16,24 +20,33 @@ class DirSplitter
 
   def split_file(file)
     print "Processing #{file}... "
-    output_directory = "#{file}_output"
+    output_directory = "#{output_directory_base}/#{file}"
 
-    if File.directory?(output_directory)
-      puts "skipped because #{output_directory} already exists."
-      next
+    splitter = Splitter.new(file, output_directory)
+
+    if splitter.has_dcim_files?
+      if File.directory?(output_directory)
+        puts "skipped because #{output_directory} already exists."
+      else
+        FileUtils.mkdir_p(output_directory)
+
+        splitter.split_to_files
+        puts 'done.'
+      end
     else
-      Dir.mkdir(output_directory)
+      puts "skipped because it doesn't contains dcim files."
     end
+  end
 
-    Splitter.new(file, output_directory).split_to_files
-    puts "done."
+  def is_dcim?(file)
+    DcimChecker.new(file).is_dcim?
   end
 end
 
 class Splitter
   SEPARATOR = "\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00DICM\x02\x00"
 
-  attr_reader :file_in_string
+  attr_reader :file_in_string, :output_dir
 
   def initialize(file, output_dir = nil)
     @file_in_string = File.binread(file)
@@ -49,13 +62,17 @@ class Splitter
     end
   end
 
+  def has_dcim_files?
+    file_parts.size > 0
+  end
+
   def file_parts
-    file_in_string.split(SEPARATOR)
+    @_file_parts ||= file_in_string.split(SEPARATOR)
   end
 end
 
-if ARGV.size == 1
-  DirSplitter.new(ARGV[0]).split_dcim_files
+if ARGV.size == 2
+  DirSplitter.new(ARGV[0], ARGV[1]).split_dcim_files
 else
-  puts "Usage: ./splitter.rb <directory>"
+  puts "Usage: ./splitter.rb <input_directory> <output_directory>"
 end
